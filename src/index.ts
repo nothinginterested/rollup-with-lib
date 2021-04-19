cd71a700985766f6980b9be6de7c0c1de5f19c92
你的名字 <>
2021-04-20 03:37:07 +0800

    feat: Object

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,34 @@
-export function reactivity() {
-    
+export const enum ReactiveFlags {
+    IS_Reactive = '__lzz_isReactive'
+}
+
+export interface Target {
+    [ReactiveFlags.IS_Reactive]?: boolean
+}
+
+
+export function reactive(target: object) {
+    const handle = (target: any, key: any) => {
+        if (key === ReactiveFlags.IS_Reactive) {
+            return true;
+        }
+        const res=Reflect.get(target,key)
+
+
+        return target[key];
+    };
+    return new Proxy(
+        target,
+        {
+            get: handle
+        }
+    );
+
+}
+
+export function isReactive(value: unknown): boolean {
+    let tag = (value as Target)[ReactiveFlags.IS_Reactive];
+
+    return !!(value && (value as Target)[ReactiveFlags.IS_Reactive] as boolean);
+
 }
